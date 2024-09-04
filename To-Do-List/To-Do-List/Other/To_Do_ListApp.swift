8855bc76464dc60e1a0a80eea29f4aa4277f9938
Kevin Yang-Li <yanglikevin@gmail.com>
2024-09-03 23:36:55 -0400

    divided the files into 4 folders

--- To-Do-List/To-Do-List/Other/To_Do_ListApp.swift
+++ To-Do-List/To-Do-List/Other/To_Do_ListApp.swift
@@ -5,10 +5,15 @@
 //  Created by Kevin Yang-Li on 8/29/24.
 //
 
+import FirebaseCore
 import SwiftUI
 
 @main
 struct To_Do_ListApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
